e97afcc2dee53a992c6a5c6199e9a9af1a9aed0b
hakubo <bukaj.kelo+github@gmail.com>
2013-04-04 15:34:22 +0200

    support for periods of time, better logging for summary

--- lib/Flurry/flurry.rb
+++ lib/Flurry/flurry.rb
@@ -48,16 +48,25 @@ class Flurry
 	end
 
 	def getData data, endPoint
-		@data[endPoint] = data['day']['@value']
+		day = data['day']
+		day = [day] if !day.is_a? Array
+
+		@data[endPoint] = day.reduce(0){|sum, val| sum + val['@value'].to_i }
 	end
 
-	def getSummary data
+	def getSummary data, endPoint
 
 		data = data['event']
 
 		if data.respond_to? 'each'
 			data.each do |event|
-				@data[event['@eventName']] = event['@usersLastDay']
+				name = event.delete '@eventName'
+
+				@keys = event.keys.map!{|key| key[1..-1]} if @keys == nil
+
+				vals = event.values
+
+				@data[name] = [nil] + vals.map{|x| x.to_i}
 			end
 		end
 	end
@@ -79,26 +88,26 @@ class Flurry
 		self.send get, data, endPoint
 	end
 
-	def getAll date = -1
+	def getAll date = -1, endDate = date
 
 		[:ActiveUsers, :PageViews, :NewUsers, :AvgSessionLength, :AvgPageViewsPerSession, :Summary].each do |endPoint|
-			puts "Fetching data for - " + endPoint.to_s
-			get endPoint, date
+			puts 'Fetching data for - ' + endPoint.to_s
+			get endPoint, date, endDate
 			# Flurry api is throttled at 1 req/sec
 			sleep 1
 		end
 
-		CSV.open "data.csv", "wb" do |csv|
+		CSV.open 'data.csv', 'wb' do |csv|
 			csv << ['//' + (Date.today + date).to_s]
 
-			csv << ['Name', 'Value']
+			csv << ['Name', 'Value'] + @keys
 
 			@data.each do |key, value|
-				csv << [key, value]
+				csv << [key, value].flatten
 			end
 		end
 	end
 end
 
-#Flurry.new(@apiKey, @androidGG).getAll
-Flurry.new(@apiKey).getApps
+Flurry.new(@apiKey, @androidGG).getAll
+#Flurry.new(@apiKey).getApps
